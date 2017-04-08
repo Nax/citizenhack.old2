101ea2807add336d357026f0f12eb595bfbc603b
Nax <max@bacoux.com>
2017-04-08 17:59:57 +0200

    Still WIP

---
+++ include/types.h
@@ -0,0 +1,9 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+#include <cstdint>
+
+using ActorRef          = uint16_t;
+using DungeonLevelRef   = uint8_t;
+
+#endif
---
+++ src/actors.cpp
@@ -0,0 +1,16 @@
+#include <actors.h>
+
+Actors::Actors() : _count(0u)
+{
+
+}
+
+Actors::~Actors()
+{
+
+}
+
+ActorRef Actors::create(ActorID actor_id, DungeonLevelRef dungeon_level, Vector2i position)
+{
+
+}
